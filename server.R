a7cd8e9070704834ecc23a076fc45c8ceac6d951
Thiemo Loos <thiemo@D-10-18-239-205.dhcp4.washington.edu>
2018-05-29 14:48:13 -0700

    Basic app format created with inputs that should be used for reactive variables

---
+++ server.R
@@ -0,0 +1,11 @@
+library(shiny)
+library(dplyr)
+library(DT)
+library(ggplot2)
+
+
+my_server <- function(input, output) {
+
+}
+
+shinyServer(my_server)
---
+++ ui.R
@@ -0,0 +1,49 @@
+library(shiny)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+
+# Creates UI layout and elements
+my_ui <- fluidPage(
+  titlePanel(strong("United States Mortality Rates Report")),
+  br(),
+  sidebarLayout(
+    sidebarPanel(
+      h3("Control Panel"),
+      selectInput("state_select",
+        label = p("Select State"),
+        choices = state.name, selected = "Alabama"
+      ),
+      sliderInput("year_slider",
+        label = "Year",
+        min = 1980, max = 2014, value = 1980
+      ),
+      selectInput("type_slider",
+        label = "Mortality Type",
+        choices = c("Alcohol use disorders", "Drugs use disorders", "Interpersonal violence", "Self-harm"),
+          selected = "Alcohol use disorders"
+      )
+    ),
+    mainPanel(
+      h3("Display Panel"),
+      tabsetPanel(
+        type = "tabs",
+        tabPanel(
+          "Question 1", br()
+        ),
+        tabPanel(
+          "Question 2", br()
+        ),
+        tabPanel(
+          "Question 3", br()
+        ),
+        tabPanel(
+          "Question 4", br()
+        )
+      )
+    )
+  )
+)
+
+shinyUI(my_ui)

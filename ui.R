fbe7ee69b0c01642066b511991da432605f5ed95
igorbanin <igor.banin@hotmail.com>
2018-05-31 13:32:09 -0700

    intro pushed

--- ui.R
+++ ui.R
@@ -30,6 +30,30 @@ my_ui <- fluidPage (
       h3("Findings Panel"),
       tabsetPanel(type = "tabs",
                   tabPanel(
+                    "Data Introduction", h4(
+                      "The data set we are using is mortality rates in the United States by substance abuse disorders
+                      and intentional injurious collected by the Institute for Health Metrics and Evaluation Institute
+                      for Health Metrics of the University of Washington. This data uses US census data for populations
+                      as a base to form mortality rates based on total individual reported cases. We will also be using
+                      the complete data set from the US census that this report uses for further demographic, socio-economic,
+                      and other features of county populations to find deeper trends. This is needed to truly find the potential
+                      reasons behind the data’s mortality rates. The main mortality data set includes information from
+                      1980-2014. This includes mortality rates by state and county. It also breaks down the causes of
+                      mortality by county with a FIPS number attached to that county. This provides vital information that
+                      will aid us in being able to visually map this data, which can help us find initial geographic trends
+                      to further explore. Past geographic data the only demographic segmentation of by gender, which is why
+                      we are including other census data. The domain knowledge for this project will be understanding the most
+                      common types of mortalities in the United States and how certain population features can impact these
+                      types in order to have a starting place for our analysis. This will be through researching causes for
+                      these types through government reports on public policy and its past effects on population health.
+                      This system will allow you to look through our analysis on four core problems listed in the next tabs.
+                      You can filter the data visualizations by state, year, and mortality where it is appropriate."
+                    ),
+                    br(),
+                    h4("Data Link: http://ghdx.healthdata.org/record/united-states-substance-use-disorders-and-intentional-injuries-mortality-rates-county-1980")
+                    
+                    ),
+                  tabPanel(
                     "Generational Data", h3("As this data set contains the lives of those in different generations is there any
                                             major difference between the mortality rates and types for each generation? What could
                                             some of the historical events or factors that could have contributed to this correlations?"),
